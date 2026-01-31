8acb89c133bdd66991238e476d62049c1df31ec2
Davis-don <davismugoikou@gmail.com>
2026-01-31 14:08:43 +0300

    added footer

--- client/src/components/Common-components/Footer.tsx
+++ client/src/components/Common-components/Footer.tsx
@@ -1,12 +1,33 @@
-import './footer.css'
+import React from 'react';
+import './footer.css';
 
-function Footer() {
+const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear();
+  
   return (
-   <div className="overall-footer-component">
-    <h1>Footer container</h1>
-    <p>This is the footer section of the layout.</p>
-   </div>
-  )
-}
+    <footer className="overall-footer-component">
+      <div className="footer-content">
+        <div className="copyright-container">
+          <div className="copyright-content">
+            <p className="copyright">
+              © {currentYear} AcadPlanner. All rights reserved.
+            </p>
+            <p className="created-by">
+              Created by{' '}
+              <a 
+                href="https://www.kinstryx.co.ke" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="company-link"
+              >
+                Kinstry Systems
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
